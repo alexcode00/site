b11e616d60a6127d7ade57a2a43cf4a91e67bb18
afkgho2 <sashabolmashov366@gmail.com>
2026-06-21 19:05:15 +0300

    fix js2

--- site/script-roof.js
+++ site/script-roof.js
@@ -93,7 +93,7 @@ if (form) {
         btn.disabled = true;
 
         try {
-            await fetch("/send", {
+            await fetch("/send", { 
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ name, phone, comment, product: selectedProduct || "Не указан" })
--- site/script.js
+++ site/script.js
@@ -84,7 +84,7 @@ if (form) {
         btn.disabled = true;
 
         try {
-            const response = await fetch("/send", {
+            const response = await fetch("/send", { 
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ name, phone, comment, product: selectedProduct || "Не указан" })
